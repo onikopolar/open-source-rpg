bab315620dfa8b6fece19671144ad133a18080bf
oniko <sangust1pc2@gmail.com>
2025-11-18 15:35:43 -0300

    corrigir redirecionamento da página inicial - usar getServerSideProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect } from 'react'
-
-import Head from 'next/head'
-import Router from 'next/router'
+import Head from 'next/head';
 
 export default function Home() {
-  useEffect(() => {
-    Router.push('/dashboard');
-  }, []);
-
   return (
     <div>
       <Head>
         <title>Home</title>
       </Head>
-
       <h1>Redirecionando...</h1>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      destination: '/dashboard',
+      permanent: false,
+    },
+  };
 }
