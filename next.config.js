77def880413b968afd3a4b7bc9158c0cd1e78c34
oniko <sangust1pc2@gmail.com>
2025-11-18 16:18:18 -0300

    desativar pré-renderização estática e ESLint no build

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@ const nextConfig = {
   webpack: (config) => {
     config.externals.push('@prisma/client')
     return config
-  }
+  },
+  // Desativa pré-renderização estática
+  experimental: {
+    esmExternals: 'loose'
+  },
+  // Força todas as páginas a serem server-rendered
+  poweredByHeader: false,
+  compress: false
 }
 
 module.exports = nextConfig
